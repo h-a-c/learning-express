b0718f9475560ed830db77d0e925b6dabdefce93
h-a-c <howching@gmail.com>
2020-05-27 01:21:51 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+let Article = require('../models/article')
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Set public folder 
+app.use(express.static(path.join(__dirname, '../public')))
+
+//Check connection
+db.once('open', function() {
+	console.log('Connected to MongoDB')
+});
+//Check db errors
+db.on('error', function(err){
+	console.log(err);
+});
 
 //Load View Engine
 app.set('views', path.join(__dirname, '../views'));
@@ -8,32 +29,46 @@ app.set('view engine', 'pug');
 
 
 app.get('/', function(req, res) {
-	let articles = [
-	{
-		id:1,
-		title: 'Article One',
-		author: 'h-a-c',
-		body: 'article one test'
-	},
-	{
-		id:2,
-		title: 'Article Two',
-		author: 'k-s',
-		body: 'article two test'
-	},
-	{
-		id:3,
-		title: 'Article Three',
-		author: 'h-a-c',
-		body: 'article three test'
-	}
-	];
-	res.render('index',{
-		title: 'title',
-		articles: articles
+	Article.find({}, function(err, articles){
+		res.render('index',{
+			title: 'title',
+			articles: articles
+		});	
 	});
+	
 });
 
+//get an article
+app.get('/article/:id', function(req, res) {
+	Article.findById(req.params.id, function(err, article){
+		if(err) {
+			console.log(err);
+		} else {
+			res.render('article', {
+				article:article
+			})
+		}
+	});
+
+})
+
+// post request
+app.post('/articles/add', function(req, res) {
+	let article = new Article();
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+	article.save(function(err){
+		if(err){
+			console.log(err)
+		} else {
+			res.redirect('/')
+		}
+	});
+	return article;
+})
+
+// get request
 app.get('/articles/add', function(req, res) {
 	res.render('add_article', {
 		title:'Add Article'
